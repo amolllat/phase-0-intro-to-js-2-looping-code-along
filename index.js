f744c033a0e3ced419aea4a5195dd8a7c0077ce6
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-19 23:23:53 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names =['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names, surprise) {
+    let myArray=[];
+    for (let i=0;i<names.length;i++){
+        let a =`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+        // console.log(a);
+        myArray.push(a);
+    }
+  return myArray;
+}
+
+function countDown(number){
+    for (let i=number; i>=0;i--){
+        console.log(i)
+    }
+}
+countDown(10);
